4ebf012dac5dff0d2c13091d3c5f88df0f889ce4
Sagar Sharma <sagarsharma708@gmail.com>
2017-05-25 20:36:30 +0530

    Calender

--- app/src/main/java/com/project/verbosetech/bustracker/fragments/Calender_Fragment.java
+++ app/src/main/java/com/project/verbosetech/bustracker/fragments/Calender_Fragment.java
@@ -13,9 +13,11 @@ import android.widget.Toast;
 
 import com.project.verbosetech.bustracker.R;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 import devs.mulham.horizontalcalendar.HorizontalCalendar;
 import devs.mulham.horizontalcalendar.HorizontalCalendarListener;
@@ -30,6 +32,10 @@ public class Calender_Fragment extends Fragment {
     private View view;
     TextView month,year;
     ImageView left,right;
+    SimpleDateFormat sdf;
+    Date cal_date;
+    Calendar startdate,endDate,prevDate;
+    HorizontalCalendar horizontalCalendar;
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
@@ -39,50 +45,41 @@ public class Calender_Fragment extends Fragment {
         left=(ImageView)view.findViewById(R.id.decrease_month);
         right=(ImageView) view.findViewById(R.id.increase_month);
 
+
+        startdate=Calendar.getInstance();
+        startdate.set(Calendar.DATE,1);
+        startdate.set(Calendar.HOUR, 00);
+        startdate.set(Calendar.MINUTE, 00);
+        startdate.set(Calendar.SECOND, 0);
+        startdate.set(Calendar.MILLISECOND, 0);
+        startdate.add(Calendar.MONTH, 0);
+
+
         /** end after 1 month from now */
-        final Calendar endDate = Calendar.getInstance();
-        endDate.add(Calendar.MONTH, 1);
-        Log.e("End Date",endDate+"");
+        endDate = Calendar.getInstance();
+        endDate.add(Calendar.MONTH,0);
+        endDate.add(Calendar.DATE,0);
 
-        /** start before 1 month from now */
-        final Calendar startDate = Calendar.getInstance();
-        startDate.add(Calendar.MONTH, -1);
-        Log.e("Start Date",startDate+"");
+//        prevDate = Calendar.getInstance();
+//        prevDate.set(Calendar.DATE,1);
+//        prevDate.set(Calendar.HOUR, 00);
+//        prevDate.set(Calendar.MINUTE, 00);
+//        prevDate.set(Calendar.SECOND, 0);
+//        prevDate.set(Calendar.MILLISECOND, 0);
+//        prevDate.add(Calendar.MONTH, -1);
 
-        Calendar calendar=Calendar.getInstance();
+        month.setText(new SimpleDateFormat("MMMM").format(startdate.getTime())+"");
+        year.setText(new SimpleDateFormat("yyyy").format(startdate.getTime())+"");
 
-        month.setText(new SimpleDateFormat("MMMM").format(calendar.getTime())+"");
-        year.setText(new SimpleDateFormat("yyyy").format(calendar.getTime())+"");
-
-        left.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Toast.makeText(getActivity(),new SimpleDateFormat("MMMM").format(startDate.getTime())+"",Toast.LENGTH_LONG).show();
-
-
-            }
-        });
-        right.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Toast.makeText(getActivity(),new SimpleDateFormat("MMMM").format(endDate.getTime())+"",Toast.LENGTH_LONG).show();
-
-            }
-        });
-
-
-
-        HorizontalCalendar horizontalCalendar = new HorizontalCalendar.Builder(view, R.id.calendarView)
-                .startDate(startDate.getTime())
+        horizontalCalendar = new HorizontalCalendar.Builder(view, R.id.calendarView)
+                .startDate(startdate.getTime())
                 .endDate(endDate.getTime())
                 .datesNumberOnScreen(7)   // Number of Dates cells shown on screen (Recommended 5)
                 .dayNameFormat("EEE")	  // WeekDay text format
                 .dayNumberFormat("dd")    // Date format
                 .monthFormat("MMM") 	  // Month format
                 .showDayName(true)	  // Show or Hide dayName text
-                .showMonthName(false)	  // Show or Hide month text
+                .showMonthName(true)	  // Show or Hide month text
                 .textColor(Color.DKGRAY, Color.BLACK)    // Text color for none selected Dates, Text color for selected Date.
                 .selectedDateBackground(Color.TRANSPARENT)  // Background color of the selected date cell.
                 .selectorColor(Color.WHITE)// Color of the selection indicator bar (default to colorAccent).
@@ -109,7 +106,85 @@ public class Calender_Fragment extends Fragment {
             }
         });
 
+
+
+
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                horizontalCalendar.selectDate(startdate.getTime(),true);
+
+            }
+        });
+
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Toast.makeText(getActivity(),new SimpleDateFormat("MMMM").format(endDate.getTime())+"",Toast.LENGTH_LONG).show();
+//                month.setText(new SimpleDateFormat("MMMM").format(endDate.getTime()));
+//                year.setText(new SimpleDateFormat("yyyy").format(endDate.getTime())+"");
+//                getMonth();
+                horizontalCalendar.selectDate(endDate.getTime(),true);
+
+            }
+        });
+
         return view;
     }
 
-}
+    public void getMonth(){
+
+        sdf = new SimpleDateFormat("MMMM", Locale.ENGLISH);
+        try {
+            cal_date = sdf.parse(month.getText().toString());
+            Log.e("Month",sdf.format(cal_date)+"");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        setMonth(cal_date);
+
+    }
+
+    public void setMonth(Date date){
+
+        startdate=Calendar.getInstance();
+        startdate.set(Calendar.DATE,1);
+        startdate.set(Calendar.HOUR, 00);
+        startdate.set(Calendar.MINUTE, 00);
+        startdate.set(Calendar.SECOND, 0);
+        startdate.set(Calendar.MILLISECOND, 0);
+        startdate.set(Calendar.MONTH, date.getMonth());
+
+
+        /** end after 1 month from now */
+        endDate = Calendar.getInstance();
+        endDate.set(Calendar.DATE,1);
+        endDate.set(Calendar.HOUR, 00);
+        endDate.set(Calendar.MINUTE, 00);
+        endDate.set(Calendar.SECOND, 0);
+        endDate.set(Calendar.MILLISECOND, 0);
+        endDate.set(Calendar.MONTH, date.getMonth()+1);
+
+        prevDate = Calendar.getInstance();
+        prevDate.set(Calendar.DATE,1);
+        prevDate.set(Calendar.HOUR, 00);
+        prevDate.set(Calendar.MINUTE, 00);
+        prevDate.set(Calendar.SECOND, 0);
+        prevDate.set(Calendar.MILLISECOND, 0);
+        prevDate.set(Calendar.MONTH,date.getMonth()-1);
+
+
+    }
+
+    public void setCalendar(Calendar startdate,Calendar endDate){
+
+
+
+
+
+}}
--- app/src/main/java/com/project/verbosetech/bustracker/fragments/DropFragment.java
+++ app/src/main/java/com/project/verbosetech/bustracker/fragments/DropFragment.java
@@ -3,16 +3,22 @@ package com.project.verbosetech.bustracker.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.project.verbosetech.bustracker.R;
+import com.project.verbosetech.bustracker.others.DelayAutoCompleteTextView;
 
 /**
  * Created by this pc on 14-05-17.
@@ -23,7 +29,13 @@ public class DropFragment extends Fragment {
     private View view;
     Button setReminder,no_button;
     AlertDialog alertDialog;
-
+    private Integer THRESHOLD = 2;
+    private DelayAutoCompleteTextView geo_autocomplete;
+    ImageView gps_search;
+    FrameLayout fm;
+    DelayAutoCompleteTextView textView;
+    LatLng user_latlang;
+    GoogleApiClient googleApiClient;
     SupportMapFragment mapFragment;
     private GoogleMap Map;
     LinearLayout linearLayout;
@@ -42,9 +54,27 @@ public class DropFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view= inflater.inflate(R.layout.location_fragment_2, container, false);
+        textView = (DelayAutoCompleteTextView) view.findViewById(R.id.geo_autocomplete);
+        gps_search = (ImageView) view.findViewById(R.id.gps_search);
+        fm = (FrameLayout) view.findViewById(R.id.layout21);
         setReminder=(Button)view.findViewById(R.id.drop_reminder);
         no_button=(Button)view.findViewById(R.id.no);
         linearLayout=(LinearLayout)view.findViewById(R.id.layout);
+
+        no_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.e("daba","ddaba");
+                linearLayout.setVisibility(View.GONE);
+
+
+            }
+        });
+
+
+
+
         setReminder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,14 +90,7 @@ public class DropFragment extends Fragment {
                     }
                 });
 
-                no_button.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
 
-                        linearLayout.setVisibility(View.GONE);
-
-                    }
-                });
 
                 AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity(),R.style.MyDialogTheme);
                 alertDialogBuilder.setView(promptsView);
@@ -89,7 +112,7 @@ public class DropFragment extends Fragment {
             mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map2);
             if (mapFragment == null) {
                 mapFragment = SupportMapFragment.newInstance();
-                getChildFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
+                getChildFragmentManager().beginTransaction().replace(R.id.map2, mapFragment).commit();
             }
             mapFragment.getMapAsync(new OnMapReadyCallback() {
                 @Override
